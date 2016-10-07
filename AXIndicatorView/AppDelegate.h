ceaef3775ffe1d8de0304292c1c44e1339da9705
devedbox <devedbox@gmail.com>
2016-10-07 17:00:31 +0800

    Initial Commit

---
+++ AXIndicatorView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AXIndicatorView
+//
+//  Created by devedbox on 2016/10/7.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
