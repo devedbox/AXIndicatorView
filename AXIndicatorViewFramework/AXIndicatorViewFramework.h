7a6ec9f5d29109a502bed32c7c0a6f1a08e675b9
aixing-hb <aixing@hydsoft.com>
2017-09-29 14:50:56 +0800

    Added codes.

---
+++ AXIndicatorViewFramework/AXIndicatorViewFramework.h
@@ -0,0 +1,19 @@
+//
+//  AXIndicatorViewFramework.h
+//  AXIndicatorViewFramework
+//
+//  Created by devedbox on 2017/9/29.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AXIndicatorViewFramework.
+FOUNDATION_EXPORT double AXIndicatorViewFrameworkVersionNumber;
+
+//! Project version string for AXIndicatorViewFramework.
+FOUNDATION_EXPORT const unsigned char AXIndicatorViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AXIndicatorViewFramework/PublicHeader.h>
+
+
